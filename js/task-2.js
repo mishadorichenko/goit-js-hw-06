22136a42c3ec38671524605d8b0c9062d53b2023
Misha Dorichenko <misha.dorichenko@gmail.com>
2024-02-16 18:03:08 +0200

    for review

---
+++ js/task-2.js
@@ -0,0 +1,31 @@
+class Storage {
+  #items = [];
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const itemNumber = this.#items.indexOf(itemToRemove);
+
+    if (itemNumber !== -1) {
+      this.#items.splice(itemNumber, 1);
+    } else {
+      console.log('Такого товару не існує!');
+    }
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
